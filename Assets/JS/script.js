08fd13a93c96fb0b9c705584ddd28911cdb14bc3
Lily Russ <llyruss@yahoo.com>
2022-09-30 12:39:56 -0500

    saved text entries to local storage

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,26 +1,64 @@
 //Add current date
-let today= moment();
+let today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do, YYYY"));
 
 // highlight timeblock by past, present, future
-let currentHour=parseInt(moment().format("H"))
+let currentHour = parseInt(moment().format("H"))
 let pageTime = $(".task")
 
-for (i=0; i<pageTime.length; i++){
- if(currentHour==pageTime[i].getAttribute("value")){
-    $(pageTime[i]).removeClass("future")
-    $(pageTime[i]).removeClass("past")
-    $(pageTime[i]).addClass("present")    
- }
- else if (currentHour < pageTime[i].getAttribute("value")){
-    $(pageTime[i]).removeClass("past")
-    $(pageTime[i]).removeClass("present") 
-    $(pageTime[i]).addClass("future")
- }
-else {
-    $(pageTime[i]).removeClass("present")
-    $(pageTime[i]).removeClass("future")
-    $(pageTime[i]).addClass("past")
-}
+for (i = 0; i < pageTime.length; i++) {
+    if (currentHour == pageTime[i].getAttribute("value")) {
+        $(pageTime[i]).removeClass("future")
+        $(pageTime[i]).removeClass("past")
+        $(pageTime[i]).addClass("present")
+    }
+    else if (currentHour < pageTime[i].getAttribute("value")) {
+        $(pageTime[i]).removeClass("past")
+        $(pageTime[i]).removeClass("present")
+        $(pageTime[i]).addClass("future")
+    }
+    else {
+        $(pageTime[i]).removeClass("present")
+        $(pageTime[i]).removeClass("future")
+        $(pageTime[i]).addClass("past")
+    }
 }
 //save text entries to localStorage 
+
+let saveButton = $(".saveBtn")
+
+for (i = 0; i < saveButton.length; i++) {
+    $(saveButton[i].firstChild).on("click", function (e) {
+        //DOM traversal!
+        let textArea = e.target.parentElement.parentElement.childNodes[3].childNodes[1].value
+        let textHour = e.target.parentElement.parentElement.childNodes[3].getAttribute('value')
+
+        taskData = JSON.parse(localStorage.getItem("taskData"));
+
+        taskData[0][textHour] = textArea
+
+        localStorage.setItem("taskData", JSON.stringify(taskData))
+    })
+}
+
+if (localStorage.getItem("taskData") === null) {
+    let taskData = [
+        {
+            "9": "",
+            "10": "",
+            "11": "",
+            "12": "",
+            "13": "",
+            "14": "",
+            "15": "",
+            "16": "",
+            "17": "",
+        }
+    ];
+    localStorage.setItem("taskData", JSON.stringify(taskData));
+}
+let storeTaskData = JSON.parse(localStorage.getItem("taskData"))
+
+
+
+
